8d420fb5676ed6c18c6e1b5860b86a10865db2cd
rydeblack <ryhad.boughanmi@gmail.com>
2025-03-31 11:30:28 +0200

    Day 01 job 1 done

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include "unistd.h"
+#include <string.h>
 
+void my_putchar(char c);
+void my_putstr(char *str);
 
 int main(void) {
-    printf("Hello, World!\n");
+
+    my_putchar('T');
+    my_putchar('\n');
+    my_putstr("Hello world\n");
     return 0;
+}
+
+
+void my_putchar(char c) {
+    write(1, &c, 1);
+}
+
+void my_putstr(char *str) {
+    write(1, str, strlen(str));
 }
---
+++ myputstr.c
@@ -0,0 +1,5 @@
+//
+// Created by ryrym_i6sf5hg on 3/31/2025.
+//
+
+#include "myputstr.h"
